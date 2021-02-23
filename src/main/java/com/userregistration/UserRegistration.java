7c69020d477982b6090ff367ac48f7ba67f3e1b5
Lavanya <lavanyace929@gmail.com>
2021-02-23 16:43:21 +0530

    Added:First Name Validation

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -1,7 +1,20 @@
 package com.userregistration;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    //First Name should start With Capital And Has Min 3 charecters
+    public static void firstNameValidity(String name) {
+        System.out.println("First Name Validity: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", name));
+
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Program");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your First name");
+        String firstName = sc.nextLine();
+        firstNameValidity(firstName);
     }
 }
+
