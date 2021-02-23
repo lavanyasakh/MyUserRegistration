59732ca3782196b6598e0b635349e2fca218ff3a
Lavanya <lavanyace929@gmail.com>
2021-02-23 17:00:00 +0530

    Added:Last Name Validation

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -9,12 +9,19 @@ public class UserRegistration {
         System.out.println("First Name Validity: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", name));
 
     }
+    //Last Name Should Start With Capital And Has Min 3 Charecters
+    public static void lastNameValidity(String name) {
+        System.out.println("Last Name: " + Pattern.matches("^[A-Z]{1}[a-zA-Z]{2,}$", name));
+    }
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Program");
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Your First name");
         String firstName = sc.nextLine();
+        System.out.println("Enter Your Last name");
+        String lastName = sc.nextLine();
         firstNameValidity(firstName);
+        lastNameValidity(lastName);
     }
 }
 
